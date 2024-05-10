bf412f5f3e8f3f2e08d342ff8032e5254fd190e6
kuzzmak <kuzminski.antonio@gmail.com>
2024-05-11 00:44:03 +0200

    Added construction of layers for model

--- model.py
+++ model.py
@@ -1,12 +1,21 @@
+from typing import List
+
+import torch
 import torch.nn as nn
 
 
 class MLP(nn.Module):
 
-    def __init__(self):
+    def __init__(self, shapes: List[int], activation: nn.Module = nn.ReLU) -> None:
         super().__init__()
 
-        self.fc1 = nn.Linear(4, 10)
+        self.act = activation()
+        self._num_layers = len(shapes) - 1
 
-    def forward(self, X):
-        return self.fc1(X)
+        for i in range(self._num_layers):
+            setattr(self, f'fc_{i}', nn.Linear(shapes[i], shapes[i + 1]))
+
+    def forward(self, x) -> torch.Tensor:
+        for i in range(self._num_layers):
+            x = getattr(self, f'fc_{i}')(x)
+        return x
