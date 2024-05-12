1a2fd3f58cc0b9540b0de6839d7305a08127e031
kuzzmak <kuzminski.antonio@gmail.com>
2024-05-12 22:13:40 +0200

    .

--- main.py
+++ main.py
@@ -52,148 +52,160 @@ def load_data(data_path: str) -> pd.DataFrame:
         return additional_fields_json.apply(
             lambda x: extract_field_from_json_object(x, key))
 
-    year_manufactured = get_series_from_additional_fields_json(
-        'yearManufactured')
-    year_manufactured.name = 'yearManufactured'
-    year_manufactured_nan_vals = year_manufactured.isnull()
-    print('number of missing values for yearManufactured:',
-          year_manufactured_nan_vals.sum())
-    print('median yearManufactured:', year_manufactured.median())
-    print('max yearManufactured:', year_manufactured.max())
-    print('min yearManufactured:', year_manufactured.min())
+    model_id = get_series_from_additional_fields_json('modelId')
+    print(len(model_id.unique()))
+    # print(model_id.max())
 
-    mileage = get_series_from_additional_fields_json('mileage')
-    mileage.name = 'mileage'
-    mileage[mileage < 0] = 0
-    mileage[mileage > 1e6] = 1e6
-    mileage_nan_vals = mileage.isnull()
-    print('number of missing values for mileage:', mileage_nan_vals.sum())
-    print('median mileage:', mileage.median())
-    print('max mileage:', mileage.max())
-    print('min mileage:', mileage.min())
+    # model_type = get_series_from_additional_fields_json('modelType')
+    # print(len(model_type.unique()))
 
-    motor_size = get_series_from_additional_fields_json('motorSize')
-    motor_size.name = 'motorSize'
-    motor_size[motor_size > 8500] = np.NaN
-    motor_size_nan_vals = motor_size.isnull()
-    print('number of missing values for motorSize:', motor_size_nan_vals.sum())
-    print('median motorSize:', motor_size.median())
-    print('max motorSize:', motor_size.max())
-    print('min motorSize:', motor_size.min())
+    # year_manufactured = get_series_from_additional_fields_json(
+    #     'yearManufactured')
+    # year_manufactured.name = 'yearManufactured'
+    # year_manufactured_nan_vals = year_manufactured.isnull()
+    # print('number of missing values for yearManufactured:',
+    #       year_manufactured_nan_vals.sum())
+    # print('median yearManufactured:', year_manufactured.median())
+    # print('max yearManufactured:', year_manufactured.max())
+    # print('min yearManufactured:', year_manufactured.min())
 
-    motor_power = get_series_from_additional_fields_json('motorPower')
-    motor_power.name = 'motorPower'
-    motor_power[motor_power > 1000] = np.NaN
-    motor_power_nan_vals = motor_power.isnull()
-    print('number of missing values for motorPower:', motor_power_nan_vals.sum())
-    print('median motoPower:', motor_power.median())
-    print('max motorPower:', motor_power.max())
-    print('min motorPower:', motor_power.min())
+    # mileage = get_series_from_additional_fields_json('mileage')
+    # mileage.name = 'mileage'
+    # mileage[mileage < 0] = 0
+    # mileage[mileage > 1e6] = 1e6
+    # mileage_nan_vals = mileage.isnull()
+    # print('number of missing values for mileage:', mileage_nan_vals.sum())
+    # print('median mileage:', mileage.median())
+    # print('max mileage:', mileage.max())
+    # print('min mileage:', mileage.min())
 
-    fuel_consumption = get_series_from_additional_fields_json(
-        'fuelConsumption')
-    fuel_consumption.name = 'fuelConsumption'
-    fuel_consumption = fuel_consumption.replace('', np.NaN)
-    fuel_consumption = fuel_consumption.astype(float)
-    fuel_consumption[fuel_consumption > 40] = np.NaN
-    fuel_consumption_nan_vals = fuel_consumption.isnull()
-    print('number of missing values for fuelConsumption:',
-          fuel_consumption_nan_vals.sum())
-    print('median fuelConsumption:', fuel_consumption.median())
-    print('max fuelConsumption:', fuel_consumption.max())
-    print('min fuelConsumption:', fuel_consumption.min())
+    # motor_size = get_series_from_additional_fields_json('motorSize')
+    # motor_size.name = 'motorSize'
+    # motor_size[motor_size > 8500] = np.NaN
+    # motor_size_nan_vals = motor_size.isnull()
+    # print('number of missing values for motorSize:', motor_size_nan_vals.sum())
+    # print('median motorSize:', motor_size.median())
+    # print('max motorSize:', motor_size.max())
+    # print('min motorSize:', motor_size.min())
 
-    co2_emission = get_series_from_additional_fields_json('co2Emission')
-    co2_emission.name = 'co2Emission'
-    co2_emission = co2_emission.replace('', np.NaN)
-    co2_emission = co2_emission.astype(float)
-    co2_emission[co2_emission > 500] = np.NaN
-    co2_emission_nan_vals = co2_emission.isnull()
-    print('number of missing values for co2Emission:',
-          co2_emission_nan_vals.sum())
-    print('median co2Emission:', co2_emission.median())
-    print('max co2Emission:', co2_emission.max())
-    print('min co2Emission:', co2_emission.min())
-    # print('10 largest:')
-    # print(co2_emission.nlargest(50))
+    # motor_power = get_series_from_additional_fields_json('motorPower')
+    # motor_power.name = 'motorPower'
+    # motor_power[motor_power > 1000] = np.NaN
+    # motor_power_nan_vals = motor_power.isnull()
+    # print('number of missing values for motorPower:', motor_power_nan_vals.sum())
+    # print('median motoPower:', motor_power.median())
+    # print('max motorPower:', motor_power.max())
+    # print('min motorPower:', motor_power.min())
 
-    transmission_type = get_series_from_additional_fields_json(
-        'transmissionTypeId')
-    transmission_type.name = 'transmissionTypeId'
-    transmission_type_nan_vals = transmission_type.isnull()
-    print('number of missing values for transmissionTypeId:',
-          transmission_type_nan_vals.sum())
+    # fuel_consumption = get_series_from_additional_fields_json(
+    #     'fuelConsumption')
+    # fuel_consumption.name = 'fuelConsumption'
+    # fuel_consumption = fuel_consumption.replace('', np.NaN)
+    # fuel_consumption = fuel_consumption.astype(float)
+    # fuel_consumption[fuel_consumption > 40] = np.NaN
+    # fuel_consumption_nan_vals = fuel_consumption.isnull()
+    # print('number of missing values for fuelConsumption:',
+    #       fuel_consumption_nan_vals.sum())
+    # print('median fuelConsumption:', fuel_consumption.median())
+    # print('max fuelConsumption:', fuel_consumption.max())
+    # print('min fuelConsumption:', fuel_consumption.min())
 
-    # | fuel_consumption_nan_vals | co2_emission_nan_vals | transmission_type_nan_vals
-    to_remove = year_manufactured_nan_vals | mileage_nan_vals | motor_size_nan_vals | motor_power_nan_vals
-    df = df[~to_remove]
+    # co2_emission = get_series_from_additional_fields_json('co2Emission')
+    # co2_emission.name = 'co2Emission'
+    # co2_emission = co2_emission.replace('', np.NaN)
+    # co2_emission = co2_emission.astype(float)
+    # co2_emission[co2_emission > 500] = np.NaN
+    # co2_emission_nan_vals = co2_emission.isnull()
+    # print('number of missing values for co2Emission:',
+    #       co2_emission_nan_vals.sum())
+    # print('median co2Emission:', co2_emission.median())
+    # print('max co2Emission:', co2_emission.max())
+    # print('min co2Emission:', co2_emission.min())
+    # # print('10 largest:')
+    # # print(co2_emission.nlargest(50))
 
-    df_train_val = df[df['condition_id'] == 20]
-    df_test = df[df['condition_id'] == 40]
+    # transmission_type = get_series_from_additional_fields_json(
+    #     'transmissionTypeId')
+    # transmission_type.name = 'transmissionTypeId'
+    # transmission_type_nan_vals = transmission_type.isnull()
+    # print('number of missing values for transmissionTypeId:',
+    #       transmission_type_nan_vals.sum())
 
-    print('number of train and val exmaples:', len(df_train_val))
-    print('number of test examples:', len(df_test))
+    # # | fuel_consumption_nan_vals | co2_emission_nan_vals | transmission_type_nan_vals
+    # to_remove = year_manufactured_nan_vals | mileage_nan_vals | motor_size_nan_vals | motor_power_nan_vals
+    # df = df[~to_remove]
 
-    # df = pd.concat([year_manufactured, mileage], axis=1)
+    # df_train_val = df[df['condition_id'] == 20]
+    # df_test = df[df['condition_id'] == 40]
 
-    year_manufactured = year_manufactured[~to_remove]
-    mileage = mileage[~to_remove]
-    motor_size = motor_size[~to_remove]
-    motor_power = motor_power[~to_remove]
-    fuel_consumption = fuel_consumption[~to_remove]
-    co2_emission = co2_emission[~to_remove]
-    transmission_type = transmission_type[~to_remove]
+    # print('number of train and val exmaples:', len(df_train_val))
+    # print('number of test examples:', len(df_test))
 
-    year_manufactured, year_manufactured_mean, year_manufactured_std = z_score(
-        year_manufactured)
-    mileage, mileage_mean, mileage_std = z_score(mileage)
-    motor_size, motor_size_mean, motor_size_std = z_score(motor_size)
-    motor_power, motor_power_mean, motor_power_std = z_score(motor_power)
-    fuel_consumption, fuel_consumption_mean, fuel_consumption_std = z_score(
-        fuel_consumption)
-    co2_emission, co2_emission_mean, co2_emission_std = z_score(co2_emission)
-    transmission_type = transmission_type.map(TRANSMISSON_MAPPING)
+    # # df = pd.concat([year_manufactured, mileage], axis=1)
 
-    year_manufactured = torch.tensor(year_manufactured.values).reshape(-1, 1)
-    mileage = torch.tensor(mileage.values).reshape(-1, 1)
-    motor_size = torch.tensor(motor_size.values).reshape(-1, 1)
-    motor_power = torch.tensor(motor_power.values).reshape(-1, 1)
-    # fuel_consumption = torch.tensor(fuel_consumption.values).reshape(-1, 1)
-    # co2_emission = torch.tensor(co2_emission.values).reshape(-1, 1)
-    # transmission_type = F.one_hot(torch.tensor(transmission_type.values))
+    # year_manufactured = year_manufactured[~to_remove]
+    # mileage = mileage[~to_remove]
+    # motor_size = motor_size[~to_remove]
+    # motor_power = motor_power[~to_remove]
+    # fuel_consumption = fuel_consumption[~to_remove]
+    # co2_emission = co2_emission[~to_remove]
+    # transmission_type = transmission_type[~to_remove]
 
-    X = torch.hstack((year_manufactured, mileage, motor_size,
-                     motor_power)).to(torch.float32)
+    # year_manufactured, year_manufactured_mean, year_manufactured_std = z_score(
+    #     year_manufactured)
+    # mileage, mileage_mean, mileage_std = z_score(mileage)
+    # motor_size, motor_size_mean, motor_size_std = z_score(motor_size)
+    # motor_power, motor_power_mean, motor_power_std = z_score(motor_power)
+    # fuel_consumption, fuel_consumption_mean, fuel_consumption_std = z_score(
+    #     fuel_consumption)
+    # co2_emission, co2_emission_mean, co2_emission_std = z_score(co2_emission)
+    # transmission_type = transmission_type.map(TRANSMISSON_MAPPING)
 
-    # plt.figure(figsize=(10, 8))
-    # sns.countplot(y=year_manufactured)
-    # sns.countplot(y=transmission_type)
-    # plt.show()
+    # year_manufactured = torch.tensor(year_manufactured.values).reshape(-1, 1)
+    # mileage = torch.tensor(mileage.values).reshape(-1, 1)
+    # motor_size = torch.tensor(motor_size.values).reshape(-1, 1)
+    # motor_power = torch.tensor(motor_power.values).reshape(-1, 1)
+    # # fuel_consumption = torch.tensor(fuel_consumption.values).reshape(-1, 1)
+    # # co2_emission = torch.tensor(co2_emission.values).reshape(-1, 1)
+    # # transmission_type = F.one_hot(torch.tensor(transmission_type.values))
 
-    return X
+    # X = torch.hstack((year_manufactured, mileage, motor_size,
+    #                  motor_power)).to(torch.float32)
+
+    # # plt.figure(figsize=(10, 8))
+    # # sns.countplot(y=year_manufactured)
+    # # sns.countplot(y=transmission_type)
+    # # plt.show()
+
+    # return X
 
 
 if __name__ == '__main__':
     data_path = r'ML_zadatak_auti.csv'
+    data = load_data(data_path)
     # df = pd.read_csv(data_path)
     # df_train_val = df[df['condition_id'] == 20]
     # df_test = df[df['condition_id'] == 40]
     # print('number of train and val exmaples:', len(df_train_val))
     # print('number of test examples:', len(df_test))
     # print('number of examples:', len(df))
-    dataset_fields = [
-        ('yearManufactured', FieldType.NUMERICAL),
-        ('mileage', FieldType.NUMERICAL),
-        ('motorSize', FieldType.NUMERICAL),
-        ('motorPower', FieldType.NUMERICAL),
-        # ('fuelConsumption', FieldType.NUMERICAL),
-        # ('co2Emission', FieldType.NUMERICAL),
-        # ('transmissionTypeId', FieldType.CATEGORICAL),
-    ]
-    datasets = get_datasets(data_path, dataset_fields)
-    for x, y in datasets['train']:
-        print(x, y)
-        break
+    # dataset_fields = [
+    #     ('yearManufactured', FieldType.NUMERICAL),
+    #     ('mileage', FieldType.NUMERICAL),
+    #     ('motorSize', FieldType.NUMERICAL),
+    #     ('motorPower', FieldType.NUMERICAL),
+    #     # ('fuelConsumption', FieldType.NUMERICAL),
+    #     # ('co2Emission', FieldType.NUMERICAL),
+    #     # ('transmissionTypeId', FieldType.CATEGORICAL),
+    #     ('manufacturerId', FieldType.CATEGORICAL)
+    # ]
+    # datasets = get_datasets(data_path, dataset_fields)
+    # for i, (x, y) in enumerate(datasets['train']):
+    #     print(x, y)
+    #     print(x.shape)
+    #     if i > 10:
+    #         break
+    #     break
     # train_val_dataset = CarAdDataset(data_path, dataset_fields, 'train_val')
     # test_dataset = CarAdDataset(data_path, dataset_fields, 'test')
 
